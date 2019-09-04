5cfab3c4935e1761d27d6a7e57f6b8a30e9ba633
Chris Smith <chris.smith@pega.com>
2019-09-04 17:34:26 -0400

    Add remote compile script

---
+++ remote-compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -e $@ ]; then
+  echo "Usage:"
+  echo ""
+  echo "  remote-compile.sh <@drush-alias>"
+  echo ""
+  exit 1
+fi
+
+echo "Compiling rdkafka.so..."
+drush $@ ssh "rm -rf /tmp/acquia-php-rdkafka"
+drush $@ ssh "git clone https://github.com/christophersmith262/acquia-php-rdkafka.git /tmp/acquia-php-rdkafka"
+drush $@ ssh "make -C /tmp/acquia-php-rdkafka clean"
+drush $@ ssh "make -C /tmp/acquia-php-rdkafka"
+
+echo "Downloading rdkafka.so..."
+drush $@ ssh "cat /tmp/acquia-php-rdkafka/dist/rdkafka.so" > rdkafka.so
+
+echo "Cleaning up..."
+drush $@ ssh "rm -rf /tmp/acquia-php-rdkafka"
+
+echo "Done!"
